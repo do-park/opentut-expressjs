a1ff29252fa5ead704f5e4cd2dab0864edd6468c
박도희 <dohee.pa@gmail.com>
2021-05-09 23:30:54 +0900

    Fix redirect routing

--- main.js
+++ main.js
@@ -72,8 +72,7 @@ app.post('/create_process', function (request, response) {
     var title = post.title;
     var description = post.description;
     fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.writeHead(302, { Location: `/?id=${title}` });
-      response.end();
+      response.redirect(`/page/${title}`);
     })
   });
 });
@@ -116,7 +115,7 @@ app.post('/update_process', function (request, response) {
     var description = post.description;
     fs.rename(`data/${id}`, `data/${title}`, function (error) {
       fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.redirect(`/?id=${title}`);
+        response.redirect(`/page/${title}`);
       })
     });
   });
